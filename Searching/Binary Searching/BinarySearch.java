f18a815b4d2063b69a5194a54724237c7a0d8ff3
jeevap0000022-hub <jeevap0000022@gmail.com>
2026-09-01 21:41:33 +0530

    Create BinarySearch.java

---
+++ Searching/Binary Searching/BinarySearch.java
@@ -0,0 +1,21 @@
+int[] arr = {10, 20, 30, 40, 50};
+int key = 40;
+
+int low = 0;
+int high = arr.length - 1;
+
+while (low <= high) {
+
+    int mid = (low+(high-low))/2;
+
+    if (arr[mid] == key) {
+        System.out.println("Found at index: " + mid);
+        break;
+    }
+    else if (arr[mid] < key) {
+        low = mid + 1;
+    }
+    else {
+        high = mid - 1;
+    }
+}
